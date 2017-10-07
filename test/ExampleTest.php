7f6ebd24c6883ee93fbdc5d95a2bca237b83a7f9
Wanderson <wandersonwhcr@gmail.com>
2017-10-07 11:27:20 -0300

    Include some example

---
+++ test/ExampleTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace FooTest\Bar;
+
+use Foo\Bar\Example;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * FooBar Example Test
+ */
+class ExampleTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->example = new Example();
+    }
+
+    public function testBazQux()
+    {
+        $this->assertNull($this->example->getBazQux());
+        $this->assertNull($this->example->setBazQux('OneTwo'));
+        $this->assertSame('OneTwo', $this->example->getBazQux());
+        $this->assertNull($this->example->setBazQux(null));
+        $this->assertNull($this->example->getBazQux());
+    }
+}
